8b6a053ff9f4f677a5be826f5c34af50131737d5
VimalParmar01 <pvimal20003@gmail.com>
2024-08-24 22:34:49 +0530

    Update keylogger.py

--- PRODIGY_CS_04/keylogger.py
+++ PRODIGY_CS_04/keylogger.py
@@ -14,17 +14,15 @@ def keylogger(key):
     with open(log_file_path, "a") as log_file:
         log_file.write(event)
 
-print("---------------- Keylogger Disclaimer ----------------")
+print("---------------- Welcome to Keylogger ----------------")
 print("\nThis keylogger program is intended for educational and ethical purposes only.")
 print("Unauthorized use, distribution, or modification of this program is strictly prohibited.")
 print("By using this program, you agree to the following terms and conditions:")
 print("\n1. You will only use this program on devices and systems for which you have explicit permission.")
-print("2. You will not use this program to violate any laws, regulations, or terms of service.")
-print("3. You will not use this program to harm, disrupt, or exploit any devices or systems.")
-print("4. You will not use this program to intercept, collect, or store any sensitive or confidential information.")
-print("5. You will not redistribute or sell this program without the express permission of the author.")
-print("6. The author is not responsible for any damages or losses incurred as a result of using this program.")
-print("7. You will respect the privacy and security of all devices and systems you interact with using this program.")
+print("2. You not use this keylogger to violate any laws:")
+print("3. You  not use this keylogger to harm, disrupt, or exploit any devices or systems.")
+print("4. You will not use this keylogger to intercept, collect,any confidential information.")
+print("5. The author is not responsible for any damages or losses incurred as a result of using this keylogger.")
 
 accept_terms = input("\nDo you accept these terms and conditions? (Yes/No): ")
 
